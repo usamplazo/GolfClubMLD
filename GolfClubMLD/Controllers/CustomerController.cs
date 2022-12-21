5786b41da2f6c7cdf4df10189fcb877ee82e204d
usamplazo <petrovicgligorijeTSI10B@gmail.com>
2022-12-22 00:38:27 +0100

    Selecting golf course term, for which we want to make appointment

--- GolfClubMLD/Controllers/CustomerController.cs
+++ GolfClubMLD/Controllers/CustomerController.cs
@@ -20,13 +20,18 @@ namespace GolfClubMLD.Controllers
         }
         [HttpGet]
         [RoleAuthorize(Roles.Customer)]
-        public async Task<ActionResult> ReserveCourse(int courseId, string selDay = "Mon")
+        public async Task<ActionResult> ReserveCourse(int courseId, string selDay)
         {
-            List<CourseTermBO> allCourseTerms = await _custRepo.GetTermsForSelCourse(courseId);
+            List<CourseTermBO> allCourseTerms;
+            allCourseTerms = await _custRepo.GetTermsForSelCourse(courseId);
+            ViewData["CourseId"] = courseId;
             if (allCourseTerms != null)
             {
-                string[] daysOfWeek = new string[7] { "Mo", "Tue", "Wen", "Thu", "Fri", "Sat", "Sun" };
+                string[] daysOfWeek = new string[7] { "Mon", "Tue", "Wen", "Thu", "Fri", "Sat", "Sun" };
                 ViewData["DaysOfWeek"] = daysOfWeek;
+                if (string.IsNullOrEmpty(selDay))
+                    selDay = "Mon";
+                ViewData["SelDay"] = selDay;
                 List<CourseTermBO> selDayTerms = new List<CourseTermBO>();
                     foreach (var ct in allCourseTerms)
                     {
